a71eeba8503c49948600df57a1fdc085a19f0d5c
Chinedu K. Eke <ekechinedu488@gmail.com>
2021-09-02 15:51:32 +0100

    added team channel component

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -40,6 +40,26 @@ function ChannelListContainer() {
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
                 <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="team" />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview {...previewProps} type="team" />
+                    )}
+                />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="messaging" />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview {...previewProps} type="messaging" />
+                    )}
+                />
             </div>
         </>
     );
